380a0497939a68a15d233535e1fc857ea56bf3ff
Shubham gupta Ggps <24877361+gptshubham595@users.noreply.github.com>
2022-11-14 13:44:37 +0530

    Add files via upload

--- app/src/main/java/com/example/activityrecognition/DetectedActivitiesIntentService.kt
+++ app/src/main/java/com/example/activityrecognition/DetectedActivitiesIntentService.kt
@@ -18,10 +18,14 @@ import com.google.android.gms.tasks.OnSuccessListener
 class DetectedActivitiesIntentService : IntentService(TAG) {
     override fun onCreate() {
         super.onCreate()
+        Log.d("MOTION","Detected Activity Created")
     }
 
     override fun onHandleIntent(intent: Intent?) {
         val result = ActivityRecognitionResult.extractResult(intent)
+        var mostActivityResult = result.mostProbableActivity
+        var activityResult:Int = mostActivityResult.type
+
         // Get the list of the probable activities associated with the current state of the
         // device. Each activity is associated with a confidence level, which is an int between
         // 0 and 100.
